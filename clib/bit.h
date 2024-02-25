6f3239c38c50066589ef43a96b72161ace85476c
taicaile <23215696+taicaile@users.noreply.github.com>
2024-02-25 12:28:29 +0800

    add clib/bit.h

---
+++ clib/bit.h
@@ -0,0 +1,9 @@
+#ifndef BIT_H
+#define BIT_H
+
+#define bitset(byte, nbit) ((byte) |= (1 << (nbit)))
+#define bitclear(byte, nbit) ((byte) &= ~(1 << (nbit)))
+#define bitflip(byte, nbit) ((byte) ^= (1 << (nbit)))
+#define bitcheck(byte, nbit) ((byte) & (1 << (nbit)))
+
+#endif /*BIT_H*/
