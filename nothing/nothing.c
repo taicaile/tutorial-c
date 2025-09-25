238c03357ba77013f1a1f4a3f88e024417239153
taicaile <23215696+taicaile@users.noreply.github.com>
2025-09-25 12:07:54 +0800

    add nothing

--- nothing/nothing.c
+++ nothing/nothing.c
@@ -1,10 +1,8 @@
 #include "nothing.h"
 
-void printArray(int *arr, int arrSize)
-{
-    for (int i = 0; i < arrSize; i++)
-    {
-        printf("%d  ", arr[i]);
-    }
-    printf("\n");
+void printArray(int *arr, int arrSize) {
+  for (int i = 0; i < arrSize; i++) {
+    printf("%d  ", arr[i]);
+  }
+  printf("\n");
 }
